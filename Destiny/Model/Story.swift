14abcc73be553639d0ac142f35343b1cadd051f8
Ata <atakanozalan@gmail.com>
2023-03-13 15:30:23 +0300

    Destiny App assets changing

---
+++ Destiny/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destiny
+//
+//  Created by Ezagor on 13.03.2023.
+//
+
+import Foundation
---
+++ Destiny/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Destiny
+//
+//  Created by Ezagor on 13.03.2023.
+//
+
+import Foundation
