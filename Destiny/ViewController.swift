6758ea25e7251b52085bd569e6472a43452933b7
Ata <atakanozalan@gmail.com>
2023-03-13 14:41:00 +0300

    Initial Commit

---
+++ Destiny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destiny
+//
+//  Created by Ezagor on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
